cd5227074f426aae860ce622c85eb924a7118f35
Strell0n <zab23011209@heartofyorkshire.ac.uk>
2025-11-21 11:43:32 +0000

    Trying to make Levers work

---
+++ Assets/Scripts/Interaction.cs
@@ -0,0 +1,49 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public interface IInteractable
+{
+    public void SwapInteract();
+}
+
+
+public class Interaction : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public Transform InteractorSource; 
+    public float Range;
+
+
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E)) 
+        {
+            RaycastHit hit;
+            // Does the ray intersect any objects excluding the player layer
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Range))
+            {
+                if (hit.collider.TryGetComponent<LeverScript>(out LeverScript lever))
+                {
+                    lever.SwapInteract();
+                }
+            }
+
+            /*Ray r = new Ray(InteractorSource.position, InteractorSource.forward);
+            if (Physics.Raycast(r, out RaycastHit hitInfo, Range))
+            {
+                if (hitInfo.collider.gameObject.TryGetComponent(out IInteractable interactObj)) 
+                {
+                    interactObj.SwapInteract();
+                }
+            }*/
+        }
+    }
+}
--- Assets/Scripts/LeverScript.cs
+++ Assets/Scripts/LeverScript.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Events;
 
 public class LeverScript : MonoBehaviour
 {
-    private bool _leverStatus;
-    private Animator _anim;
+    public bool _leverStatus;
+    public Animator _anim;
+    [SerializeField] public UnityEvent _event;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,16 +21,36 @@ public class LeverScript : MonoBehaviour
         
     }
 
-    public void InteractWithLever()
+    public void SwapInteract()
     {
-        if (_leverStatus == false)
+        _leverStatus = !_leverStatus;
+        Debug.Log(_leverStatus + " Lever");            
+        _anim.SetBool("LeverStatus", _leverStatus);
+
+        //_event.Invoke();
+        //_leverStatus = !_leverStatus;
+
+
+
+        /*if (_leverStatus == false)
         {
-            Debug.Log(_leverStatus);
+            Debug.Log(_leverStatus);            
+            _anim.SetBool("LeverStatus", true);
+            _event.Invoke();
+            _leverStatus = !_leverStatus;
         }
 
-        else
+        else if (_leverStatus == true)
         {
             Debug.Log(_leverStatus);
-        }
+            _anim.SetBool("LeverStatus", false);
+            
+            _leverStatus = !_leverStatus;
+        }*/
     }
+
+     /*void IInteractable.interact()
+    {
+        interact();
+    }*/
 }
